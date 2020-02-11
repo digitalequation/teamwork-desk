bd0a93a242dbbfe79542d502f8d3078798b1f1ea
Robert-Cristian Chiribuc <robert.chiribuc@thebug.ro>
2020-02-11 12:09:10 +0200

    [Contracts/TicketRepository.php] add _construct

--- src/Contracts/TicketRepository.php
+++ src/Contracts/TicketRepository.php
@@ -3,11 +3,18 @@
 namespace Digitalequation\TeamworkDesk\Contracts;
 
 use Digitalequation\TeamworkDesk\Http\Requests\TicketRequest;
+use DigitalEquation\TeamworkDesk\Services\Tickets;
 use Illuminate\Http\Request;
 use Illumintate\Contracts\Auth\Authenticatable;
 
 interface TicketRepository
 {
+    /**
+     * TicketRepository constructor.
+     * @param Tickets $tickets
+     */
+    public function __construct(Tickets $tickets);
+
     /**
      * Create a new ticket.
      *
