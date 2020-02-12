8887bf3b417c75a6a7d45288b5874632e405ac98
Robert-Cristian Chiribuc <robert.chiribuc@thebug.ro>
2020-02-12 10:50:22 +0200

    [routes/api.php] use default laravel router

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
-AdvancedRoute::controllers([
-    'tickets' => TeamworkDeskAPIController::class,
-]);
+use Illuminate\Support\Facades\Route;
+
+Route::get('tickets', 'TeamworkDeskAPIController@getIndex');
+Route::get('tickets/{id}', 'TeamworkDeskAPIController@getTicket');
+Route::post('tickets', 'TeamworkDeskAPIController@postIndex');
+Route::post('tickets/reply', 'TeamworkDeskAPIController@postReply');
+Route::post('tickets/upload', 'TeamworkDeskAPIController@postUpload');
