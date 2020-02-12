e9fb65788851d518695f43c8bd88c28229bda674
Robert-Cristian Chiribuc <robert.chiribuc@thebug.ro>
2020-02-12 11:31:03 +0200

    [routes/api.php] fix route for single ticket

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('tickets', 'TeamworkDeskAPIController@getIndex');
-Route::get('tickets/{id}', 'TeamworkDeskAPIController@getTicket');
+Route::get('tickets/ticket/{id}', 'TeamworkDeskAPIController@getTicket');
 Route::post('tickets', 'TeamworkDeskAPIController@postIndex');
 Route::post('tickets/reply', 'TeamworkDeskAPIController@postReply');
 Route::post('tickets/upload', 'TeamworkDeskAPIController@postUpload');
