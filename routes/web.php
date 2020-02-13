192644de2443e62fed8838aa365c5f22cbcd4e1e
Robert-Cristian Chiribuc <robert.chiribuc@thebug.ro>
2020-02-13 11:54:31 +0200

    [routes/web.php] fixed controller name

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('/tdhook/priority', 'TeamworkWebhookController@postPriority');
-Route::post('/tdhook/status', 'TeamworkWebhookController@postStatus');
-Route::post('/tdhook/delete', 'TeamworkWebhookController@postDelete');
-Route::post('/tdhook/reply', 'TeamworkWebhookController@postReply');
-Route::post('/tdhook/note', 'TeamworkWebhookController@postNote');
+Route::post('/tdhook/priority', 'TeamworkDeskWebhookController@postPriority');
+Route::post('/tdhook/status', 'TeamworkDeskWebhookController@postStatus');
+Route::post('/tdhook/delete', 'TeamworkDeskWebhookController@postDelete');
+Route::post('/tdhook/reply', 'TeamworkDeskWebhookController@postReply');
+Route::post('/tdhook/note', 'TeamworkDeskWebhookController@postNote');
