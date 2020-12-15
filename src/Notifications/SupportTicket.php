f4b880d61fc7849baf16a911ccf499da9021e6c7
chiribuc <robert.chiribuc@thebug.ro>
2020-12-15 13:31:38 +0200

    [Notifications][SupportTicket] simplify

--- src/Notifications/SupportTicket.php
+++ src/Notifications/SupportTicket.php
@@ -2,7 +2,7 @@
 
 namespace DigitalEquation\TeamworkDesk\Notifications;
 
-use App\Notifications\{DigitalNotification, DigitalChannel};
+use DigitalEquation\TeamworkDesk\Contracts\Repositories\TicketRepository;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
@@ -11,71 +11,35 @@ class SupportTicket extends Notification
 {
     use Queueable;
 
-    protected $content;
+    protected TicketRepository $ticket;
 
-    /**
-     * Create a new notification instance.
-     *
-     * @param $content
-     */
-    public function __construct($content)
+
+    public function __construct(TicketRepository $ticket)
     {
-        $this->content = $content;
+        $this->ticket = $ticket;
     }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @param mixed $notifiable
-     *
-     * @return array
-     */
     public function via($notifiable): array
     {
-        return [DigitalChannel::class];
+        return ['database'];
     }
 
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param mixed $notifiable
-     *
-     * @return MailMessage
-     */
     public function toMail($notifiable): MailMessage
     {
         return (new MailMessage)
-            ->line($this->content['body'])
-            ->action($this->content['action_text'], $this->content['action_url'])
+            ->line($this->ticket['body'])
+            ->action($this->ticket['action_text'], $this->ticket['action_url'])
             ->line('Thank you for using our application!');
     }
 
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param mixed $notifiable
-     *
-     * @return array
-     */
     public function toArray($notifiable): array
     {
         return [
-            //
+            'icon'        => 'fa-info',
+            'body'        => $this->ticket['body'],
+            'from'        => $notifiable,
+            'action_text' => $this->ticket['action_text'],
+            'action_url'  => $this->ticket['action_url'],
         ];
-    }
-
-    /**
-     * Get the Digital representation of the notification
-     *
-     * @param mixed $notifiable
-     *
-     * @return DigitalNotification
-     */
-    public function toDigitalApp($notifiable): DigitalNotification
-    {
-        return (new DigitalNotification)
-            ->icon('fa-info')
-            ->action($this->content['action_text'], $this->content['action_url'])
-            ->body($this->content['body']);
     }
 }
