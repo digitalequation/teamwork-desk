e89a85f7fca50a63e679298ba39d430b2d3b4121
chiribuc <robert.chiribuc@thebug.ro>
2020-12-15 12:47:20 +0200

    [routes][api] add routes to protected middleware

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('tickets', 'TeamworkDeskAPIController@getIndex');
-Route::get('tickets/ticket/{id}', 'TeamworkDeskAPIController@getTicket');
-Route::post('tickets', 'TeamworkDeskAPIController@postIndex');
-Route::post('tickets/reply', 'TeamworkDeskAPIController@postReply');
-Route::post('tickets/upload', 'TeamworkDeskAPIController@postUpload');
+Route::group(['middleware' => config('teamwork-desk.authorization')], function () {
+    Route::get('tickets', 'TeamworkDeskAPIController@getIndex');
+    Route::get('tickets/ticket/{id}', 'TeamworkDeskAPIController@getTicket');
+    Route::post('tickets', 'TeamworkDeskAPIController@postIndex');
+    Route::post('tickets/reply', 'TeamworkDeskAPIController@postReply');
+    Route::post('tickets/upload', 'TeamworkDeskAPIController@postUpload');
+});
