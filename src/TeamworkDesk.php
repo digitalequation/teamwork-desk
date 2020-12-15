86f4994780a7aa2e88e4d0abe4fb0d5dd4a2c275
chiribuc <robert.chiribuc@thebug.ro>
2020-12-15 12:44:11 +0200

    [TeamworkDesk] cleanup

--- src/TeamworkDesk.php
+++ src/TeamworkDesk.php
@@ -4,18 +4,5 @@ namespace DigitalEquation\TeamworkDesk;
 
 class TeamworkDesk
 {
-    /**
-     * Indicated if Teamwork Desk migrations will be run.
-     */
-    public static bool $runsMigrations = true;
-
-    /**
-     * Configure Teamwork Desk to not register its migrations.
-     */
-    public static function ignoreMigrations(): TeamworkDesk
-    {
-        static::$runsMigrations = false;
-
-        return new static;
-    }
+    //
 }
