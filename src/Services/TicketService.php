619bdee1f22995fa10e63a79e8423ba346de8ccb
chiribuc <robert.chiribuc@thebug.ro>
2020-12-15 12:44:39 +0200

    [TicketService] update class references + standardize response

--- src/Services/TicketService.php
+++ src/Services/TicketService.php
@@ -2,15 +2,15 @@
 
 namespace DigitalEquation\TeamworkDesk\Services;
 
-use DigitalEquation\TeamworkDesk\Exceptions\TeamworkHttpException;
-use DigitalEquation\TeamworkDesk\Exceptions\TeamworkInboxException;
-use DigitalEquation\TeamworkDesk\Exceptions\TeamworkParameterException;
-use DigitalEquation\TeamworkDesk\Exceptions\TeamworkUploadException;
+use DigitalEquation\TeamworkDesk\Exceptions\TeamworkDeskHttpException;
+use DigitalEquation\TeamworkDesk\Exceptions\TeamworkDeskInboxException;
+use DigitalEquation\TeamworkDesk\Exceptions\TeamworkDeskJsonException;
+use DigitalEquation\TeamworkDesk\Exceptions\TeamworkDeskParameterException;
 use GuzzleHttp\Client;
-use GuzzleHttp\Exception\ClientException;
-use GuzzleHttp\Psr7\Response;
-use GuzzleHttp\Psr7\Stream;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\File;
+use Psr\Http\Message\StreamInterface;
 
 class TicketService
 {
@@ -28,248 +28,120 @@ class TicketService
         }
     }
 
-    /**
-     * Get tickets priorities.
-     *
-     * @return array
-     * @throws TeamworkHttpException
-     */
-    public function priorities(): array
+    public function priorities(): ?array
     {
         try {
-            /** @var Response $response */
-            $response = $this->client->get('ticketpriorities.json');
-            /** @var Stream $body */
-            $body = $response->getBody();
-
-            return json_decode($body->getContents(), true);
-        } catch (ClientException $e) {
-            throw new TeamworkHttpException($e->getMessage(), 400);
+            return $this->getResponse(
+                $this->client->get('ticketpriorities.json')->getBody()
+            );
+        } catch (GuzzleException $e) {
+            throw new TeamworkDeskHttpException($e->getMessage(), 400);
         }
     }
 
-    /**
-     * Get a list of tickets for a customer.
-     *
-     * @param int $customerId
-     *
-     * @return array
-     * @throws TeamworkHttpException
-     */
-    public function customer($customerId): array
+    public function customer(int $customerId): array
     {
         try {
-            /** @var Response $response */
-            $response = $this->client->get(sprintf('customers/%s/previoustickets.json', $customerId));
-            /** @var Stream $body */
-            $body = $response->getBody();
-
-            return json_decode($body->getContents(), true);
-        } catch (ClientException $e) {
-            throw new TeamworkHttpException($e->getMessage(), 400);
+            return $this->getResponse(
+                $this->client->get(sprintf('customers/%s/previoustickets.json', $customerId))->getBody()
+            );
+        } catch (GuzzleException $e) {
+            throw new TeamworkDeskHttpException($e->getMessage(), 400);
         }
     }
 
-    /**
-     * Send a ticket to teamwork desk.
-     *
-     * @param array $data
-     *
-     * @return array
-     * @throws TeamworkHttpException
-     */
-    public function post($data): array
+    public function post(array $data): array
     {
         try {
-            /** @var Response $response */
-            $response = $this->client->post('tickets.json', [
-                'form_params' => $data,
-            ]);
-
-            /** @var Stream $body */
-            $body = $response->getBody();
-
-            return json_decode($body->getContents(), true);
-        } catch (ClientException $e) {
-            throw new TeamworkHttpException($e->getMessage(), 400);
+            return $this->getResponse(
+                $this->client->post('tickets.json', ['form_params' => $data,])->getBody()
+            );
+        } catch (GuzzleException $e) {
+            throw new TeamworkDeskHttpException($e->getMessage(), 400);
         }
     }
 
-    /**
-     * Post a reply to a ticket.
-     *
-     * @param array $data
-     *
-     * @return array
-     * @throws TeamworkHttpException
-     * @throws TeamworkParameterException
-     */
     public function reply(array $data): array
     {
         if (empty($data['ticketId'])) {
-            throw new TeamworkParameterException('The `reply` method expects the passed array param to contain `ticketId`', 400);
+            throw new TeamworkDeskParameterException('The `reply` method expects the passed array param to contain `ticketId`', 400);
         }
 
         try {
-            /** @var Response $response */
-            $response = $this->client->post(sprintf('tickets/%s.json', $data['ticketId']), [
-                'form_params' => $data,
-            ]);
-
-            /** @var Stream $body */
-            $body = $response->getBody();
-
-            return json_decode($body->getContents(), true);
-        } catch (ClientException $e) {
-            throw new TeamworkHttpException($e->getMessage());
+            return $this->getResponse(
+                $this->client->post(sprintf('tickets/%s.json', $data['ticketId']), ['form_params' => $data,])
+                    ->getBody()
+            );
+        } catch (GuzzleException $e) {
+            throw new TeamworkDeskHttpException($e->getMessage());
         }
     }
 
-    /**
-     * Get ticket by id.
-     *
-     * @param int $ticketId
-     *
-     * @return array
-     * @throws TeamworkHttpException
-     */
-    public function ticket($ticketId): array
+    public function ticket(int $ticketId): array
     {
         try {
-            /** @var Response $response */
-            $response = $this->client->get(sprintf('tickets/%s.json', $ticketId));
-            /** @var Stream $body */
-            $body = $response->getBody();
-
-            return json_decode($body->getContents(), true);
-        } catch (ClientException $e) {
-            throw new TeamworkHttpException($e->getMessage(), 400);
+            return $this->getResponse(
+                $this->client->get(sprintf('tickets/%s.json', $ticketId))->getBody()
+            );
+        } catch (GuzzleException $e) {
+            throw new TeamworkDeskHttpException($e->getMessage(), 400);
         }
     }
 
-    /**
-     * Return an inbox by name.
-     *
-     * @param string $name
-     *
-     * @return array
-     * @throws TeamworkHttpException
-     * @throws TeamworkInboxException
-     */
     public function inbox(string $name): array
     {
         try {
-            /** @var Response $response */
-            $response = $this->client->get('inboxes.json');
-            /** @var Stream $body */
-            $body    = $response->getBody();
-            $inboxes = json_decode($body->getContents(), true);
-
-            $inbox = collect($inboxes['inboxes'])->first(
-                function ($inbox) use ($name) {
-                    return $inbox['name'] === $name;
-                }
-            );
+            $inboxes = $this->getResponse($this->client->get('inboxes.json')->getBody());
+            $inbox   = collect($inboxes['inboxes'])->first(fn($inbox) => $inbox['name'] === $name);
 
             if (!$inbox) {
-                throw new TeamworkInboxException("No inbox found with the name: $name!", 400);
+                throw new TeamworkDeskInboxException("No inbox found with the name: $name!", 400);
             }
 
             return $inbox;
-        } catch (ClientException $e) {
-            throw new TeamworkHttpException($e->getMessage(), 400);
+        } catch (GuzzleException $e) {
+            throw new TeamworkDeskHttpException($e->getMessage(), 400);
         }
     }
 
-    /**
-     * Get teamwork desk inboxes.
-     *
-     * @return array
-     * @throws TeamworkHttpException
-     */
     public function inboxes(): array
     {
         try {
-            /** @var Response $response */
-            $response = $this->client->get('inboxes.json');
-            /** @var Stream $body */
-            $body = $response->getBody();
-
-            return json_decode($body->getContents(), true);
-        } catch (ClientException $e) {
-            throw new TeamworkHttpException($e->getMessage(), 400);
+            return $this->getResponse($this->client->get('inboxes.json')->getBody());
+        } catch (GuzzleException $e) {
+            throw new TeamworkDeskHttpException($e->getMessage(), 400);
         }
     }
 
-    /**
-     * Return the current client info.
-     *
-     * @return array
-     * @throws TeamworkHttpException
-     */
     public function me(): array
     {
         try {
-            /** @var Response $response */
-            $response = $this->client->get('me.json');
-            /** @var Stream $body */
-            $body = $response->getBody();
-
-            return json_decode($body->getContents(), true);
-        } catch (ClientException $e) {
-            throw new TeamworkHttpException($e->getMessage(), 400);
+            return $this->getResponse($this->client->get('me.json')->getBody());
+        } catch (GuzzleException $e) {
+            throw new TeamworkDeskHttpException($e->getMessage(), 400);
         }
     }
 
-    /**
-     * Update the customer, based on customerId.
-     *
-     * @param array $data = ['customerId', 'email', 'firstName', 'lastName', 'phone', 'mobile'];
-     *
-     * @return array
-     * @throws TeamworkHttpException
-     */
-    public function postCustomer($data): array
+    public function postCustomer(array $data): array
     {
         try {
-            /** @var Response $response */
-            $response = $this->client->put('customers/' . $data['customerId'] . '.json', [
-                'json' => $data,
-            ]);
-
-            /** @var Stream $body */
-            $body = $response->getBody();
-
-            return json_decode($body->getContents(), true);
-        } catch (ClientException $e) {
-            throw new TeamworkHttpException($e->getMessage(), 400);
+            return $this->getResponse(
+                $this->client->put('customers/' . $data['customerId'] . '.json', ['json' => $data,])->getBody()
+            );
+        } catch (GuzzleException $e) {
+            throw new TeamworkDeskHttpException($e->getMessage(), 400);
         }
     }
 
-    /**
-     * Upload file to teamwork desk.
-     *
-     * @param $userId
-     * @param $file
-     *
-     * @return array
-     * @throws TeamworkHttpException
-     * @throws TeamworkUploadException
-     */
-    public function upload($userId, $file): array
+    public function upload(int $userId, UploadedFile $file): array
     {
-        if (empty($file)) {
-            throw new TeamworkUploadException('No file provided.', 400);
-        }
-
         $filename  = $file->getClientOriginalName();
         $extension = $file->getClientOriginalExtension();
         $path      = sys_get_temp_dir();
         $temp      = $file->move($path, $filename);
-        $stream    = fopen($temp->getPathName(), 'r');
+        $stream    = fopen($temp->getPathName(), 'rb');
 
         try {
-            /** @var Response $response */
             $response = $this->client->post('upload/attachment', [
                 'multipart' => [
                     [
@@ -281,9 +153,8 @@ class TicketService
                     ],
                 ],
             ]);
-            /** @var Stream $body */
-            $body = $response->getBody();
-            $body = json_decode($body->getContents(), true);
+            $body     = $response->getBody();
+            $body     = $this->getResponse($body);
 
             if (!empty($stream)) {
                 File::delete($temp->getPathName());
@@ -296,8 +167,17 @@ class TicketService
                 'name'      => $body['attachment']['filename'],
                 'size'      => $body['attachment']['size'],
             ];
-        } catch (ClientException $e) {
-            throw new TeamworkHttpException($e->getMessage(), 400);
+        } catch (GuzzleException $e) {
+            throw new TeamworkDeskHttpException($e->getMessage(), 400);
+        }
+    }
+
+    private function getResponse(StreamInterface $body)
+    {
+        try {
+            return json_decode($body->getContents(), true, 512, JSON_THROW_ON_ERROR);
+        } catch (\JsonException $e) {
+            throw new TeamworkDeskJsonException($e->getMessage());
         }
     }
 }
